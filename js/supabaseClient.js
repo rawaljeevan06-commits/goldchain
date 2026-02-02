4f95ed96b126df84310420aafbfdbd9b59eab60e
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-02 17:50:49 +0545

    supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,11 +1,10 @@
 // js/supabaseClient.js
 // Creates ONE global Supabase client
 
-const SUPABASE_URL = "https://isyaavusunsombknvhsz.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeWFhdnVzdW5zb21ia252aHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDExOTEsImV4cCI6MjA4NTUxNzE5MX0.SrOKP_Xm2Joi9QPStyAYFGmziavkXvpXbgGfG1LtEfA";
+const SUPABASE_URL = "https://isyavaausunsombknvhsz.supabase.co";
+const SUPABASE_ANON_KEY =   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeWFhdnVzdW5zb21ia252aHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDExOTEsImV4cCI6MjA4NTUxNzE5MX0.SrOKP_Xm2Joi9QPStyAYFGmziavkXvpXbgGfG1LtEfA";
 
-window.sb = window.supabase.createClient(
+window.sb = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
