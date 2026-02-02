2c64957be01654f02e916ad76644db98985f8b78
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-02 15:39:10 +0545

    supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -2,6 +2,7 @@
 // Creates ONE global Supabase client (window.sb)
 
 const SUPABASE_URL = "https://isyavausunsombknvhsz.supabase.co";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE"; // put your real anon key
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeWFhdnVzdW5zb21ia252aHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDExOTEsImV4cCI6MjA4NTUxNzE5MX0.SrOKP_Xm2Joi9QPStyAYFGmziavkXvpXbgGfG1LtEfA"; // put your real anon key
 
-window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.sb = window.supabase.createClient(https://isyavausunsombknvhsz.supabase.co, 
+                                         eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeWFhdnVzdW5zb21ia252aHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDExOTEsImV4cCI6MjA4NTUxNzE5MX0.SrOKP_Xm2Joi9QPStyAYFGmziavkXvpXbgGfG1LtEfA);
