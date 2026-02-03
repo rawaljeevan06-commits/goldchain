4f6b8b9a63b984a796c025160816d565db8e5251
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-03 10:23:51 +0545

    supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -2,7 +2,7 @@
 // Creates ONE global Supabase client
 
 const SUPABASE_URL = "https://isyavaausunsombknvhsz.supabase.co";
-const SUPABASE_ANON_KEY =   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeWFhdnVzdW5zb21ia252aHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDExOTEsImV4cCI6MjA4NTUxNzE5MX0.SrOKP_Xm2Joi9QPStyAYFGmziavkXvpXbgGfG1LtEfA";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeWFhdnVzdW5zb21ia252aHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDExOTEsImV4cCI6MjA4NTUxNzE5MX0.SrOKP_Xm2Joi9QPStyAYFGmziavkXvpXbgGfG1LtEfA";
 
 window.sb = supabase.createClient(
   SUPABASE_URL,
