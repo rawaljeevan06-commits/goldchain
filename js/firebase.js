dac8f45cf5ed7b9c0f1aaa8320d03e6aae3382d4
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-04 09:38:27 +0545

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,16 @@
 // js/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// Firebase init (global)
 
 const firebaseConfig = {
   apiKey: "AIzaSyBrNIJ-NAMZ1iSnib8zPLDqICBBawL3GRM",
   authDomain: "goldchain-e5470.firebaseapp.com",
   projectId: "goldchain-e5470",
-  storageBucket: "goldchain-e5470.appspot.com",
+  storageBucket: "goldchain-e5470.firebasestorage.app",
   messagingSenderId: "614150237166",
   appId: "1:614150237166:web:c3e9b7f8549a78e5a87cff"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+firebase.initializeApp(firebaseConfig);
+window.fbAuth = firebase.auth();
+
+console.log("Firebase ready ✅");
