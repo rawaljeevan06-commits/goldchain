5251aaff260c074ad9538cc0a95e30012a27fc5f
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-04 12:03:41 +0545

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,5 @@
-// js/firebase.js (Firebase v10 module)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBrNIJ-NAMZ1iSnib8zPLDqICBBawL3GRM",
@@ -11,10 +10,7 @@ const firebaseConfig = {
   appId: "1:614150237166:web:c3e9b7f8549a78e5a87cff"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-// keep login after refresh
-await setPersistence(auth, browserLocalPersistence);
-
-console.log("Firebase initialized ✅");
+console.log("✅ firebase.js loaded and exported auth");
