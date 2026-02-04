2bedab23134a1819915dff7343e349b16a0ab936
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-04 09:14:06 +0545

    Delete js/supabaseClient.js

--- js/supabaseClient.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = "https://isyaavausunsombknvhsz.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeWFhdnVzdW5zb21ia252aHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDExOTEsImV4cCI6MjA4NTUxNzE5MX0.SrOKP_Xm2Joi9QPStyAYFGmziavkXvpXbgGfG1LtEfA";
-
-window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-console.log("Supabase client ready:", !!window.sb);
