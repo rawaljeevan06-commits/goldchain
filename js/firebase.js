0fbe89e078c068e330a0ddc6b2f8d18a274e0254
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-04 08:54:05 +0545

    firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrNIJ-NAMZ1iSnib8zPLDqICBBawL3GRM",
+  authDomain: "goldchain-e5470.firebaseapp.com",
+  projectId: "goldchain-e5470",
+  storageBucket: "goldchain-e5470.firebasestorage.app",
+  messagingSenderId: "614150237166",
+  appId: "1:614150237166:web:c3e9b7f8549a78e5a87cff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
