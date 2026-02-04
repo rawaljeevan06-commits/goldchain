2b54b42e21f6aefafb494ee37f902869c0308185
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-04 11:21:30 +0545

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,20 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+<!-- Firebase (compat) -->
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBrNIJ-NAMZ1iSnib8zPLDqICBBawL3GRM",
-  authDomain: "goldchain-e5470.firebaseapp.com",
-  projectId: "goldchain-e5470",
-  storageBucket: "goldchain-e5470.firebasestorage.app",
-  messagingSenderId: "614150237166",
-  appId: "1:614150237166:web:c3e9b7f8549a78e5a87cff"
-};
+<!-- your firebase config file -->
+<script src="js/firebase.js"></script>
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// ✅ Must be global so signup.js can use it
-window.app = app;
-window.auth = auth;
-
-console.log("Firebase initialized ✅");
+<!-- signup logic -->
+<script src="js/signup.js"></script>
+</body>
+</html>
