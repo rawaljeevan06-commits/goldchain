623ce236fd746e84281b20c59e9c6a91a9f7351d
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-04 17:09:54 +0545

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrNIJ-NAMZ1iSnib8zPLDqICBBawL3GRM",
-  authDomain: "goldchain-e5470.firebaseapp.com",
-  projectId: "goldchain-e5470",
-  storageBucket: "goldchain-e5470.firebasestorage.app",
-  messagingSenderId: "614150237166",
-  appId: "1:614150237166:web:c3e9b7f8549a78e5a87cff"
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 
-console.log("firebase ready");
+// 🔒 IMPORTANT — FIX SAFARI
+await setPersistence(auth, browserLocalPersistence);
