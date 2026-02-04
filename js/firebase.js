11ae33d0d2bc6c81ebc8ec73dac86059b5bd0e2c
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-04 18:16:21 +0545

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,17 @@
-// js/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrNIJ-NAMZ1iSnib8PLDqICBBawL3GRM",
+  apiKey: "AIzaSyBrNIJ-NAMZ1iSnib8zPLDqICBBawL3GRM",
   authDomain: "goldchain-e5470.firebaseapp.com",
   projectId: "goldchain-e5470",
-  storageBucket: "goldchain-e5470.appspot.com",
+  storageBucket: "goldchain-e5470.firebasestorage.app",
   messagingSenderId: "614150237166",
   appId: "1:614150237166:web:c3e9b7f8549a78e5a87cff"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
