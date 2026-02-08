fa4f19fe6544fe6549549d9b9c77e649b79d0956
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-08 20:18:56 +0545

    config.js

---
+++ js/config.js
@@ -0,0 +1,11 @@
+// js/config.js
+window.GC_CONFIG = {
+  whatsappNumber: "9779800000000",     // <-- change this
+  whatsappMessage: "Hello GoldChain! I need support.",
+
+  supportEmail: "support@goldchain.com", // <-- change this
+  supportPhone: "+977-98XXXXXXXX",       // optional
+
+  officeLocationText: "Kathmandu, Nepal", // <-- change this
+  officeMapLink: "https://maps.google.com/?q=Kathmandu%20Nepal" // optional
+};
