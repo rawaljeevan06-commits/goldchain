66b87d9eee6a2762d92e45d8fe5076a6730ccf0b
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-12 09:56:19 +0545

    config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 // js/config.js
 window.GC_CONFIG = {
-  whatsappNumber: "9715400000000",     // <-- change this
+  whatsappNumber: "971540000000", // change this
   whatsappMessage: "Hello GoldChain! I need support.",
 
-  supportEmail: "supgoldchain1@gmail.com", // <-- change this
-  supportPhone: "+971-54XXXXXXXX",       // optional
+  supportEmail: "supgoldchain1@gmail.com", // change this
+  supportPhone: "+971-54XXXXXXX", // optional
 
-  officeLocationText: "Abudhabi, UAE", // <-- change this
-  officeMapLink: "hofficeMapLink: "https://maps.google.com/?q=Abu+Dhabi+UAE"" // optional
+  officeLocationText: "Abu Dhabi, UAE", // change this
+  officeMapLink: "https://maps.google.com/?q=Abu+Dhabi+UAE" // optional
 };
