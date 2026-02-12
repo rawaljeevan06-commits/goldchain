605711cb164eda632f2c3db3308a80133b33a7ae
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-12 10:11:55 +0545

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -5,14 +5,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrNIJ-NAMZ1iSnib8zPLDqICBBawL3GRM",
+   apiKey: "AIzaSyBrNIJ-NAMZ1iSnib8zPLDqICBBawL3GRM",
   authDomain: "goldchain-e5470.firebaseapp.com",
   projectId: "goldchain-e5470",
   storageBucket: "goldchain-e5470.firebasestorage.app",
   messagingSenderId: "614150237166",
   appId: "1:614150237166:web:c3e9b7f8549a78e5a87cff"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
