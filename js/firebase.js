05d26e5017c69df85924c209f01e20aaff8db533
rawaljeevan06-commits <rawal.jeevan06@gmail.com>
2026-02-12 09:48:47 +0545

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,14 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// ✅ Replace these with YOUR Firebase project config
 const firebaseConfig = {
-  apiKey: "PASTE_YOURS",
-  authDomain: "PASTE_YOURS",
-  projectId: "PASTE_YOURS",
-  storageBucket: "PASTE_YOURS",
-  messagingSenderId: "PASTE_YOURS",
-  appId: "PASTE_YOURS",
+  apiKey: "YOUR_REAL_API_KEY",
+  authDomain: "YOUR_REAL_AUTH_DOMAIN",
+  projectId: "YOUR_REAL_PROJECT_ID",
+  storageBucket: "YOUR_REAL_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_REAL_SENDER_ID",
+  appId: "YOUR_REAL_APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
